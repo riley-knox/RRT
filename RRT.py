ce5cff1a4ebb26ccd5010191692b1a2e29b4c64a
Riley Knox <rileyknox2020@u.northwestern.edu>
2019-09-13 02:56:40 -0500

    Calculations complete + scatter plot implemented
    Moved blank 'distances' list to "i" for loop in order to clear every new point
      Line 35
    Updated x- and y-distance calculation to allow for negative distances
      Lines 37-8
    Creates vector from closest node to random point
      Lines 45-6
    Calculates unit vector
      Lines 47-8
    Determines parent node
      DOES NOT ADD TO POINTDATA LIST
      Line 50
    Determines coordinates of new node
      Lines 52-4
    Adds new node name and coordinates to list of points
      Lines 56-7
    Creates scatter plot of calculated nodes
      NOT FINISHED, NEEDS AXIS SCALING
      Lines 60-4

--- RRT.py
+++ RRT.py
@@ -3,15 +3,15 @@ from __future__ import print_function
 
 #import required modules
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import imageio
 import math
 
 #program parameters
-k = 2               #number of iterations
+k = 5               #number of iterations
 d = 1               #step length
-pointsList = []    #blank list to hold point data
-distances = []      #blank list to hold point-to-point distances
+pointsList = []     #blank list to hold point data
 
 #define distance calculation function
 def calcDist(x,y):
@@ -31,17 +31,34 @@ for i in range(1,k+1):
     print(pointName)       #CAN DELETE ONCE LOOP WORKS
     randomX = np.random.randint(0,101)      #coordinates of random point
     randomY = np.random.randint(0,101)
-    print(randomX,randomY)  #CAN DELETE ONCE LOOP WORKS
+    print("coordinates of random point: ",randomX,randomY)  #CAN DELETE ONCE LOOP WORKS
+    distances = []                      #blank list to hold point-to-point distances; clears each iteration
     for j in range(len(pointsList)):
-        distX = abs(randomX-pointsList[j][1][0])
-        distY = abs(randomY-pointsList[j][1][1])
-        #print(distX,distY)  #CAN DELETE ONCE LOOP WORKS
+        distX = randomX-pointsList[j][1][0]
+        distY = randomY-pointsList[j][1][1]
+        print("coordinate distances from point{}: ".format(j),distX,distY)  #CAN DELETE ONCE LOOP WORKS
         distance = calcDist(distX,distY)
         distances.append(distance)
-    print(distances)        #CAN DELETE ONCE LOOP WORKS
-    closestIndex = np.where(distances == np.amin(distances))[0]     #very closely taken from: https://thispointer.com/numpy-amin-find-minimum-value-in-numpy-array-and-its-index/
-    print(closestIndex)     #CAN DELETE ONCE LOOP WORKS
-
-    pointData = [pointName,(randomX,randomY)]   #CHANGE TO COORDINATES OF ADDED POINT, NOT RANDOM
+    print("point-to-point distances: ",distances)        #CAN DELETE ONCE LOOP WORKS
+    closestIndex = int(np.where(distances == np.amin(distances))[0])     #very closely taken from: https://thispointer.com/numpy-amin-find-minimum-value-in-numpy-array-and-its-index/
+    print("index of closest point: ",closestIndex)     #CAN DELETE ONCE LOOP WORKS
+    closestVector = [distX,distY]           #vector from closest point to random point
+    print("vector from closest point: ",closestVector)    #CAN DELETE ONCE LOOP WORKS
+    unitVector = [dist/distance for dist in closestVector]
+    print("unit vector: ",unitVector)       #CAN DELETE ONCE LOOP WORKS
+    print("closest point + coordinates: ",pointsList[closestIndex])
+    parent = "q{}".format(closestIndex)
+    print("parent point: ",parent)
+    pointX = pointsList[closestIndex][1][0] + unitVector[0]
+    pointY = pointsList[closestIndex][1][1] + unitVector[1]
+    pointCoords = (pointX,pointY)
+    print("new point coordinates: ",pointCoords)      #CAN DELETE ONCE LOOP WORKS
+    pointData = [pointName,pointCoords]   #CHANGE TO COORDINATES OF ADDED POINT, NOT RANDOM
     pointsList.append(pointData)
-print(pointsList)
+print("point list: ",pointsList)
+
+#scatter plot of points
+pointsX = [pointsList[i][1][0] for i in range(len(pointsList))]
+pointsY = [pointsList[i][1][1] for i in range(len(pointsList))]
+plt.scatter(pointsX,pointsY)
+plt.show()
