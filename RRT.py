046b49d9aade1499ba961df39dcc8a5bf1433b92
Riley Knox <rileyknox2020@u.northwestern.edu>
2019-09-13 23:28:18 -0500

    Step 1 completed
    Adds parent point to each pointsList data entry
      Lines 67-8
    Implements pprint to display pointsList in aesthetically pleasing manner
      Lines 73-4
    Set scatter plot axis limits
      Lines 82-3

--- RRT.py
+++ RRT.py
@@ -7,11 +7,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import imageio
 import math
+import pprint
 
 #program parameters
-k = 5               #number of iterations
-d = 1               #step length
-pointsList = []     #blank list to hold point data
+k = 50              #number of iterations
+d = 1               #defined step length; not used as variable as all steps are unit vectors (have inherent length = 1)
+pointsList = []     #blank list to hold point data; has form ['point name', point coordinates, 'parent point']
 
 #define distance calculation function
 def calcDist(x,y):
@@ -22,43 +23,62 @@ def calcDist(x,y):
 startX = 50         #x coordinate
 startY = 50         #y coordinate
 startName = 'q0'    #name
-startData = [startName,(startX,startY)]
+startData = [startName,(startX,startY)] #pointsList data entry
 pointsList.append(startData)
 
 #find closest node
 for i in range(1,k+1):
-    pointName = "q{}".format(i)            #name of point being determined
-    print(pointName)       #CAN DELETE ONCE LOOP WORKS
-    randomX = np.random.randint(0,101)      #coordinates of random point
+    #name of point being determined
+    pointName = "q{}".format(i)
+    #coordinates of random point
+    randomX = np.random.randint(0,101)
     randomY = np.random.randint(0,101)
-    print("coordinates of random point: ",randomX,randomY)  #CAN DELETE ONCE LOOP WORKS
-    distances = []                      #blank list to hold point-to-point distances; clears each iteration
+    #blank list to hold point-to-point distances; clears each iteration
+    distances = []
+
+    #calculate distance from random point to each node
     for j in range(len(pointsList)):
+        #coordinate distances between random point and each successive node
         distX = randomX-pointsList[j][1][0]
         distY = randomY-pointsList[j][1][1]
-        print("coordinate distances from point{}: ".format(j),distX,distY)  #CAN DELETE ONCE LOOP WORKS
+        #calculate normalized distance from random point to node ("node distance")
         distance = calcDist(distX,distY)
+        #add individual node distance to list of node distances
         distances.append(distance)
-    print("point-to-point distances: ",distances)        #CAN DELETE ONCE LOOP WORKS
-    closestIndex = int(np.where(distances == np.amin(distances))[0])     #very closely taken from: https://thispointer.com/numpy-amin-find-minimum-value-in-numpy-array-and-its-index/
-    print("index of closest point: ",closestIndex)     #CAN DELETE ONCE LOOP WORKS
-    closestVector = [distX,distY]           #vector from closest point to random point
-    print("vector from closest point: ",closestVector)    #CAN DELETE ONCE LOOP WORKS
+
+    #determine parent point - the node to which the random point is closest
+    #very closely taken from: https://thispointer.com/numpy-amin-find-minimum-value-in-numpy-array-and-its-index/
+    closestIndex = int(np.where(distances == np.amin(distances))[0])
+
+    #create vector from closest point to random point
+    closestVector = [distX,distY]
+
+    #calculate unit vector
     unitVector = [dist/distance for dist in closestVector]
-    print("unit vector: ",unitVector)       #CAN DELETE ONCE LOOP WORKS
-    print("closest point + coordinates: ",pointsList[closestIndex])
+
+    #create parent point data entry
     parent = "q{}".format(closestIndex)
-    print("parent point: ",parent)
+
+    #determine coordinates of new node
     pointX = pointsList[closestIndex][1][0] + unitVector[0]
     pointY = pointsList[closestIndex][1][1] + unitVector[1]
     pointCoords = (pointX,pointY)
-    print("new point coordinates: ",pointCoords)      #CAN DELETE ONCE LOOP WORKS
-    pointData = [pointName,pointCoords]   #CHANGE TO COORDINATES OF ADDED POINT, NOT RANDOM
-    pointsList.append(pointData)
-print("point list: ",pointsList)
 
-#scatter plot of points
+    #create data entry for most recent point
+    pointData = [pointName,pointCoords,parent]
+
+    #add most recent point data to point data list
+    pointsList.append(pointData)
+
+#print point data in an aesthetically pleasing manner
+pprint.pprint(pointsList)
+
+#plot points in scatter plot
+#generate x- and y-values
 pointsX = [pointsList[i][1][0] for i in range(len(pointsList))]
 pointsY = [pointsList[i][1][1] for i in range(len(pointsList))]
 plt.scatter(pointsX,pointsY)
+#set axis limits
+plt.xlim(0,100)
+plt.ylim(0,100)
 plt.show()
